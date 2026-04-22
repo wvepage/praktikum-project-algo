74b73041478a247a32d181af3293664c64ad7be8
143FAW <124250143@student.upnyk.ac.id>
2026-04-22 16:45:01 +0700

    Add Koleksi struct for book collection
    Define a struct for a collection of books with properties.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdio>
 
+using namespace std;
+
+struct Koleksi {
+    string judul;
+    string penulis;
+    string kategori; // "Buku" atau "Novel"
+    int tahunTerbit;
+    float rating;
+    
+    Koleksi* prev;
+    Koleksi* next;
+};
+
+// Pointer Global
+Koleksi* head = NULL;
+Koleksi* tail = NULL;
