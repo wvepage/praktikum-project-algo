c03626b43ec5582de8e4c1d6f1ed1ffcbc8d4c2c
143FAW <124250143@student.upnyk.ac.id>
2026-04-22 16:46:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,3 +19,61 @@ struct Koleksi {
 // Pointer Global
 Koleksi* head = NULL;
 Koleksi* tail = NULL;
+
+void muatDataDariFile() {
+    FILE* file = fopen("katalog_pribadi.txt", "r");
+    if (file == NULL) return; 
+
+    char bufferJudul[200], bufferPenulis[200], bufferKategori[50];
+    int thn;
+    float rtg;
+
+    // Parsing data dengan pemisah delimiter |
+    while (fscanf(file, "%[^|]|%[^|]|%[^|]|%d|%f\n", bufferJudul, bufferPenulis, bufferKategori, &thn, &rtg) == 5) {
+        
+        Koleksi* baru = new Koleksi;
+        baru->judul = bufferJudul;
+        baru->penulis = bufferPenulis;
+        baru->kategori = bufferKategori;
+        baru->tahunTerbit = thn;
+        baru->rating = rtg;
+        baru->prev = NULL;
+        baru->next = NULL;
+
+        // Sisip Akhir (Insert Last)
+        if (head == NULL) {
+            head = tail = baru;
+        } else {
+            tail->next = baru;
+            baru->prev = tail;
+            tail = baru;
+        }
+    }
+    fclose(file);
+}
+
+void simpanDataKeFile() {
+    FILE* file = fopen("katalog_pribadi.txt", "w");
+    if (file == NULL) {
+        cout << "\n[!] Error: Tidak dapat membuat file penyimpanan.\n";
+        return;
+    }
+
+    Koleksi* bantu = head;
+    
+    // Traversal untuk menulis data
+    while (bantu != NULL) {
+        fprintf(file, "%s|%s|%s|%d|%.1f\n", 
+                bantu->judul.c_str(), 
+                bantu->penulis.c_str(), 
+                bantu->kategori.c_str(), 
+                bantu->tahunTerbit, 
+                bantu->rating);
+            
+        // Mencegah Memory Leak
+        Koleksi* hapus = bantu;
+        bantu = bantu->next;
+        delete hapus; 
+    }
+    fclose(file);
+}
